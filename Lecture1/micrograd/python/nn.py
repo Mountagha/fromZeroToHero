9fbe0660de5ec65fe75c564c25d3aa49616318aa
Mamadou Mountagha BAH <muntaghaba@gmail.com>
2023-03-05 23:55:18 -0500

    to save my strike

--- Lecture1/micrograd/python/nn.py
+++ Lecture1/micrograd/python/nn.py
@@ -2,11 +2,19 @@ import random
 from micrograd.python.engine import Value
 
 class Module:
-    pass
+    
+    def zero_grad(self):
+        for p in self.parameters():
+            p.grad = 0
+    
+    def parameters():
+        return []
 
 class Neuron(Module):
-    def __init__(self) -> None:
-        pass
+    def __init__(self, nin, nonlin=True) -> None:
+        self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
+        self.b = Value(0)
+        self.nonlin = nonlin
 
 class Layer(Module):
     pass
