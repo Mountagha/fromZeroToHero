d11153bbe48936d61a4586faa4f2639cec5d9cc9
Mamadou Mountagha BAH <muntaghaba@gmail.com>
2023-03-09 01:02:55 -0500

    finish adding nn engine

--- Lecture1/micrograd/python/nn.py
+++ Lecture1/micrograd/python/nn.py
@@ -27,7 +27,7 @@ class Neuron(Module):
         return f"{'ReLU' if self.nonlin else 'Linear'}Neuron({len(self.w)})"
 
 class Layer(Module):
-    def __init__(self, nin, nout **kwargs) -> None:
+    def __init__(self, nin, nout, **kwargs) -> None:
         self.neurons = [Neuron(nin, **kwargs) for _ in range(nout)]
 
     def __call__(self, x): 
@@ -41,4 +41,18 @@ class Layer(Module):
         return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
 
 class MLP(Module):
-    pass
+    
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layers = [Layer(sz[i], sz[i+1], nonlin=i!=len(nouts)-1) for i in range(len(nouts))]
+
+        def __call__(self, x):
+            for layer in self.layers:
+                x = layer(x)
+            return x
+
+        def parameters(self):
+            return [p for layer in self.layers for p in layer.parameters()]
+        
+        def __repr__(self):
+            return f"MLP of [{', '.join(str(layer) for layer in self.layers)}]"
