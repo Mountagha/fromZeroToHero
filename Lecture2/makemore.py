78517accf498e9b797feaa18bcfef193cda61be0
Mamadou Mountagha BAH <muntaghaba@gmail.com>
2023-03-22 23:06:11 -0400

    Adding some code to makemore

--- Lecture2/makemore.py
+++ Lecture2/makemore.py
@@ -11,3 +11,32 @@ Changes from minGPT:
   and are not weight decayed. I don't think this should make a massive 
   difference at the scale that we operate on here.
 """
+
+import os
+import sys
+import time
+import math
+import argparse
+from dataclasses import dataclass
+from typing import List
+
+import torch
+import torch.n as nn
+from torch.nn import functional as F
+from torch.utils.data import Dataset
+from torch.utils.data.dataloader import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+# ----------------------------------------------------------
+
+@dataclass
+class ModelConfig:
+    block_size: int = None # length of the input sequences of integers
+    vocab_size: int = None # the input integers are in range [0 .. vocab_size - 1]
+    # parameters below control the sizes of each model slightly differently
+    n_layer: int = 4
+    n_embd: int = 64
+    n_emb2: int = 64
+    n_head: int = 4
+
+# ---------------------------------------------------------
