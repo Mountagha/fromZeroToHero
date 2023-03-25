3d309c10547faa8f1356e5ddeb4642e3394ed838
Mamadou Mountagha BAH <muntaghaba@gmail.com>
2023-03-25 15:50:45 -0400

    adding lecture 3

--- Lecture1/micrograd/python/setup.py
+++ Lecture1/micrograd/python/setup.py
@@ -1,7 +1,8 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# with open("README.md", "r") as fh:
+#    long_description = fh.read()
+long_description = ""
 
 setuptools.setup(
     name="micrograd",
--- Lecture1/micrograd/python/tests/test_engine.py
+++ Lecture1/micrograd/python/tests/test_engine.py
@@ -1,5 +1,5 @@
 import torch
-from python.micrograd.engine import Value
+from micrograd.engine import Value
 
 def test_sanity_check():
 
