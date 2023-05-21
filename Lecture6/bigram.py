4e1b3ec12590f9fdcccc561e4ce77d10f2406d51
Mamadou Mountagha BAH <muntaghaba@gmail.com>
2023-05-20 20:37:49 -0400

    v1: Single Head attention

--- Lecture6/bigram.py
+++ Lecture6/bigram.py
@@ -161,12 +161,3 @@ for iter in range(max_iters):
 # generate from the model 
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
 print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
-
-
-
-
-
-# create pytorch optimizer
-
-
-
