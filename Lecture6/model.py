78aaf894defa94a63bf8f416488d285e3ebb3b35
Mamadou Mountagha BAH <muntaghaba@gmail.com>
2023-06-07 23:22:55 -0400

    on nano GPT model

--- Lecture6/model.py
+++ Lecture6/model.py
@@ -1,3 +1,19 @@
+"""
+Full definition of a GPT Language Model, all of it in this single file.
+References:
+1) the official GPT-2 TensorFlow implementation released by OpenAI:
+https://github.com/openai/gpt-2/blob/master/src/model.py
+2) huggingface/transformers PyTorch implementation:
+https://github.com/huggingface/transformers/blob/main/src/transformers/models/gpt2/modeling_gpt2.py
+"""
+
+def new_gelu(x):
+    """
+    Implementations of the GELU activation function currently in Google BERT repo (identical to OpenAI GPT).
+    References: Gaussian Error Linear Units (GELU) paper: https://arxiv.org.org/abs/1606.08415
+    """
+    return 0.5 * x * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (x + 0.044715 + torch.pow(x, 3.0))))
+
 import math 
 import inspect
 from dataclasses import dataclass
