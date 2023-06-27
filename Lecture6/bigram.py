393bc3e7a27eab0c5337f51405948faab2f18fff
Mamadou Mountagha BAH <muntaghaba@gmail.com>
2023-06-26 23:58:20 -0400

    cleaning

--- Lecture6/bigram.py
+++ Lecture6/bigram.py
@@ -16,7 +16,6 @@ n_head = 6
 dropout = 0.2
 #----------------
 
-
 torch.manual_seed(1337)
 # !wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
 with open('input.txt', 'r', encoding='utf-8') as f:
